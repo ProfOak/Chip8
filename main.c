10e324769dfea8bf70374b3ae63209d2b7e979bd
ProfOak <OpenProfOak@gmail.com>
2016-07-20 01:22:51 -0500

    command line file loading

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include "chip8.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+int file_exists (char *f) {
+    return access(f, F_OK) != -1;
+}
 
-    char *filename = "PONG";
+int main(int argc, char *argv[]) {
+
+    if ( argc != 2 || !file_exists(argv[1]) ) {
+        printf("Invalid file\n");
+        return 1;
+    }
+
+    char * filename = malloc(16);
+    strncpy(filename,  argv[1], strlen(argv[1]));
+
     c8_init(filename);
+
+    free(filename);
 
     for (int i = 0; i < 15; i++) {
         // emu one cycle at a time
@@ -12,3 +28,4 @@ int main(void) {
 
     return 0;
 }
+
